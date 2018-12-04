bc8f0d37d5ce89c6dd83e62d4e1b29fd6f07bc47
Berkowitz <adb16130@d71h75.pr.uconn.edu>
2018-12-04 08:55:44 -0500

    finished module 7

--- sick-fits/frontend/components/Search.js
+++ sick-fits/frontend/components/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Downshift from 'downshift';
+import Downshift, { resetIdCounter } from 'downshift';
 import Router from 'next/router';
 import { ApolloConsumer } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -22,6 +22,16 @@ const SEARCH_ITEMS_QUERY = gql`
   }
 `
 
+// go the the single page of an item when it's selected.
+function routeToItem(item) {
+  Router.push({
+    pathname: '/item',
+    query: {
+      id: item.id
+    }
+  })
+}
+
 class AutoComplete extends Component {
   state = {
     items: [],
@@ -42,35 +52,71 @@ class AutoComplete extends Component {
     })
   }, 350)
   render() {
+    resetIdCounter()
     return (
       <SearchStyles>
-        <div>
-          {/* 
-            ApolloConsumer allows for making queries on the Apollo Client and not on page load.
-            for search, the pattern <Query>{() => <input />}</Query> won't work
-          */}
-          <ApolloConsumer>
-            {
-              (client) => (
-                <input type="search" onChange={(evt) => {
-                  evt.persist();
-                  this.onChange(evt, client);
-                }} />
-              )
-            }
-          </ApolloConsumer>
-          <DropDown>
-            {
-              this.state.items.map(item => {
-                return (
-                  <DropDownItem key={item.id}>
-                    <img width="50" src={item.image} alt={item.title} />
-                  </DropDownItem>
-                )
-              })
-            }
-          </DropDown>
-        </div>
+        {/* 
+          By default downshift turns the results into strings.
+          that initally makes the item return [object Object]
+          so you need to use the title property of the item
+        */}
+        <Downshift
+          onChange={routeToItem} 
+          itemToString={item => (item === null ? '' : item.title)}>
+          {
+            ({ getInputProps, getItemProps, isOpen, inputValue, highlightedIndex }) => (
+
+            <div>
+              {/* 
+                ApolloConsumer allows for making queries on the Apollo Client and not on page load.
+                for search, the pattern <Query>{() => <input />}</Query> won't work
+              */}
+              <ApolloConsumer>
+                {
+                  (client) => (
+                    <input 
+                      {...getInputProps({
+                          type: "search",
+                          placeholder: 'Search for an item',
+                          id: 'search',
+                          className: this.state.loading ? 'loading' : '',
+                          onChange: (evt) => {
+                            evt.persist();
+                            this.onChange(evt, client);
+                          } 
+                        })
+                      } />
+                  )
+                }
+              </ApolloConsumer>
+              {/* 
+                isOpen manages if the dropdown should be visible or not
+              */}
+              { isOpen && (
+                <DropDown>
+                  {
+                    this.state.items.map((item, index) => {
+                      return (
+                        <DropDownItem 
+                          {...getItemProps({ item }) }
+                          // highlighted prop is used in the styles
+                          highlighted={index === highlightedIndex}
+                          key={item.id}>
+                          <img width="50" src={item.image} alt={item.title} />
+                          {item.title}
+                        </DropDownItem>
+                      )
+                    })
+                  }
+                </DropDown>
+              )}
+              {!this.state.items.length && !this.state.loading && (
+                <DropDownItem>Nothing found for {inputValue}</DropDownItem>
+              )}
+            </div>
+            )
+          }
+        </Downshift>
       </SearchStyles>
     )
 
